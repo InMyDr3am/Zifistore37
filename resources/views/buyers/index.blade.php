6e129e6dcd3fc66f9f1d338d889eb441a5cd8c7f
Fauzi Ramdhani <fauzi.thc@gmail.com>
2025-02-09 22:35:38 +0700

    add seeder, delete table product condition, etc

--- resources/views/buyers/index.blade.php
+++ resources/views/buyers/index.blade.php
@@ -1,28 +1,24 @@
 <x-layout>
     {{-- <x-slot:title>{{ $title }}</x-slot:title> --}}
-    <h1 class="page-title">Let's start bro!</h1>
+    <h1 class="page-title">Data Buyers</h1>
     <div class="row">
         <div class="col-md-12">
-            <div>
-                <h3 class="text-center my-4">Tutorial Laravel 11 untuk Pemula</h3>
-                <h5 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h5>
-                <hr>
-            </div>
             <div class="card border-0 shadow-sm rounded">
                 <div class="card-body">
-                    <a href="{{ route('buyers.create') }}" class="btn btn-md btn-success mb-3">ADD PRODUCT</a>
+                    <a href="{{ route('buyers.create') }}" class="btn btn-md btn-success mb-3">ADD BUYER</a>
                     <table class="table table-bordered">
                         <thead>
                             <tr>
-                                <th scope="col">name</th>
-                                <th scope="col">phone</th>
-                                <th scope="col">address</th>
-                               
+                                <th scope="col">Name</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Address</th>
                                 <th scope="col" style="width: 20%">ACTIONS</th>
                             </tr>
                         </thead>
                         <tbody>
                             @forelse ($buyers as $buyer)
+                                @include('buyers.m-edit')
+                                @include('buyers.m-delete')
                                 <tr>
                                     {{-- <td class="text-center">
                                         <img src="{{ asset('/storage/products/'.$product->image) }}" class="rounded" style="width: 150px">
@@ -31,13 +27,21 @@
                                     <td>{{ $buyer->phone }}</td>
                                     <td>{{ $buyer->address }}</td>
                                     <td class="text-center">
-                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('buyers.destroy', $buyer->id) }}" method="POST">
+                                        {{-- <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('buyers.destroy', $buyer->id) }}" method="POST">
                                             <a href="{{ route('buyers.show', $buyer->id) }}" class="btn btn-sm btn-dark">SHOW</a>
                                             <a href="{{ route('buyers.edit', $buyer->id) }}" class="btn btn-sm btn-primary">EDIT</a>
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
-                                        </form>
+                                            <button type="submit" class="btn btn-sm btn-danger">DELETE</button>
+                                        </form> --}}
+                                        <button class="btn btn-danger btn-sm" title="Hapus Data" 
+                                            data-toggle="modal" data-target="#modal-deleteBuyer{{ $buyer->id }}"> 
+                                            <i class="fas fa-regular fa-trash"></i>
+                                        </button>
+                                        <button class="btn btn-primary btn-sm" title="Edit Data"
+                                            data-toggle="modal" data-target="#modal-editBuyer{{ $buyer->id }}">
+                                            <i class="fas fa-duotone fa-pen"></i>
+                                        </button>
                                     </td>
                                 </tr>
                             @empty
@@ -52,4 +56,4 @@
             </div>
         </div>
     </div>
-  </x-layout>
+</x-layout>
