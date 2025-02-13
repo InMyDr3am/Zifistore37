015443351da15abe5b339e43b570ed51f5124970
Fauzi Ramdhani <fauzi.thc@gmail.com>
2025-02-13 17:26:29 +0700

    add seeder product

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductsSeeder extends Seeder
 {
@@ -12,6 +13,41 @@ class ProductsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('products')->insert([
+            [
+                'name' => 'Dimsum Ayam',
+                'product_category_id' => 3,
+                'product_brand_id' => 4,
+                'stock' => 100,
+                'min_stock' => 4,
+                'cost_price' => 2000,
+                'offline_price' => 2500,
+                'marketplace_price' => 3000,
+
+            ],
+            [
+                'name' => 'Dimsum Udang',
+                'product_category_id' => 3,
+                'product_brand_id' => 4,
+                'stock' => 100,
+                'min_stock' => 4,
+                'cost_price' => 2000,
+                'offline_price' => 2500,
+                'marketplace_price' => 3000,
+
+            ],
+            [
+                'name' => 'Dimsum Jamur',
+                'product_category_id' => 3,
+                'product_brand_id' => 4,
+                'stock' => 100,
+                'min_stock' => 4,
+                'cost_price' => 2000,
+                'offline_price' => 2500,
+                'marketplace_price' => 3000,
+
+            ],
+            
+        ]);
     }
 }
